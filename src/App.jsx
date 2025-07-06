c0041e28d4a495d9e4e604970f93f0fb69fe09e7
wasiff <wasifgee0012@gmail.com>
2025-07-06 19:56:23 +0500

    Initializing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
