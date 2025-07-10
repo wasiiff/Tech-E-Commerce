8d09ae8f4d43550513b6792a9b5a925975b7ced0
wasiff <wasifgee0012@gmail.com>
2025-07-10 13:07:30 +0500

    Initializing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='w-full py-6 shadow-2l shadow-black bg-white'>
+      
+    </div>
+  )
+}
